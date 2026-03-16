62f0ae1aa082fbc44ab5c51dcd83ee89e87ea572
Joud2003 <joud.salhi10@gmail.com>
2026-03-16 20:32:47 +0000

    Rename self to robot

--- nav_with_me/nav_with_me/controller.py
+++ nav_with_me/nav_with_me/controller.py
@@ -1,8 +1,5 @@
 from math import pi
-
 import numpy as np
-
-import rclpy
 
 
 def normalize_angle(angle):
@@ -13,97 +10,97 @@ def normalize_angle(angle):
     return angle
 
 
-def handleControl(self, msg):
-    front_dist = np.mean(self.front_readings)
-    self.pd_x.setPoint(front_dist)
-    if front_dist < self.wall_threshold:
+def handleControl(robot, msg):
+    front_dist = np.mean(robot.front_readings)
+    robot.pd_x.setPoint(front_dist)
+    if front_dist < robot.wall_threshold:
         msg.linear.x = 0.0
-        self.vel_pub.publish(msg)
-    if self.state == "FORWARD":
-        self.node.get_logger().info("Forward state")
+        robot.vel_pub.publish(msg)
+    if robot.state == "FORWARD":
+        # robot.node.get_logger().info("Forward state")
 
-        if front_dist > self.wall_threshold:
-            linear_v = self.pd_x.update(self.wall_threshold)
-            if abs(linear_v) > self.forward_speed:
-                linear_v = self.forward_speed * np.sign(linear_v)
+        if front_dist > robot.wall_threshold:
+            linear_v = robot.pd_x.update(robot.wall_threshold)
+            if abs(linear_v) > robot.forward_speed:
+                linear_v = robot.forward_speed * np.sign(linear_v)
             msg.linear.x = abs(linear_v)
             msg.angular.z = 0.0
-            self.vel_pub.publish(msg)
+            robot.vel_pub.publish(msg)
 
         else:
             msg.linear.x = 0.0
-            self.vel_pub.publish(msg)
+            robot.vel_pub.publish(msg)
 
-            self.target_theta = normalize_angle(
-                self.pose.theta + self.sweep_direction * (pi / 2)
+            robot.target_theta = normalize_angle(
+                robot.pose.theta + robot.sweep_direction * (pi / 2)
             )
-            self.pd_theta.setPoint(self.target_theta)
-            self.node.get_logger().info("Now switching to first turn state")
-            self.state = "TURN_1"
+            robot.pd_theta.setPoint(robot.target_theta)
+            # robot.node.get_logger().info("Now switching to first turn state")
+            robot.state = "TURN_1"
 
-    elif self.state == "TURN_1":
-        self.node.get_logger().info("First turn state")
-        error = normalize_angle(self.target_theta - self.pose.theta)
+    elif robot.state == "TURN_1":
+        # robot.node.get_logger().info("First turn state")
+        error = normalize_angle(robot.target_theta - robot.pose.theta)
         if abs(error) > 0.05:
-            msg.angular.z = max(min(self.pd_theta.update(self.pose.theta), 0.3), -0.2)
+            msg.angular.z = max(min(robot.pd_theta.update(robot.pose.theta), 0.3), -0.2)
             msg.linear.x = 0.0
-            self.vel_pub.publish(msg)
+            robot.vel_pub.publish(msg)
 
         else:
             msg.angular.z = 0.0
-            self.vel_pub.publish(msg)
-            self.start_x = self.pose.x
-            self.start_y = self.pose.y
-            self.pd_x.setPoint(self.pose.x + self.lane_width)
-            self.node.get_logger().info(f"Now switching to shift state")
-            self.state = "SHIFT"
+            robot.vel_pub.publish(msg)
+            robot.start_x = robot.pose.x
+            robot.start_y = robot.pose.y
+            robot.pd_x.setPoint(robot.pose.x + robot.lane_width)
+            # robot.node.get_logger().info(f"Now switching to shift state")
+            robot.state = "SHIFT"
 
-    elif self.state == "SHIFT":
-        self.node.get_logger().info("Shift state")
-        dx = self.pose.x - self.start_x
-        dy = self.pose.y - self.start_y
+    elif robot.state == "SHIFT":
+        # robot.node.get_logger().info("Shift state")
+        dx = robot.pose.x - robot.start_x
+        dy = robot.pose.y - robot.start_y
         dist = np.sqrt(dx**2 + dy**2)
 
-        error = self.lane_width - dist
+        error = robot.lane_width - dist
         if abs(error) > 0.05:
-            linear_v = self.pd_x.update(dist)
-            if abs(linear_v) > self.forward_speed:
-                linear_v = self.forward_speed * np.sign(linear_v)
+            linear_v = robot.pd_x.update(dist)
+            if abs(linear_v) > robot.forward_speed:
+                linear_v = robot.forward_speed * np.sign(linear_v)
 
             msg.linear.x = abs(linear_v)
             msg.angular.z = 0.0
-            self.vel_pub.publish(msg)
+            robot.vel_pub.publish(msg)
 
         else:
             msg.linear.x = 0.0
             msg.angular.z = 0.0
-            self.vel_pub.publish(msg)
-            self.target_theta = -1 * normalize_angle(
-                self.pose.theta + self.sweep_direction * (pi / 2)
+            robot.vel_pub.publish(msg)
+            robot.target_theta = -1 * normalize_angle(
+                robot.pose.theta + robot.sweep_direction * (pi / 2)
             )
-            self.pd_theta.setPoint(self.target_theta)
-            self.node.get_logger().info("Now switching to second turn state")
+            robot.pd_theta.setPoint(robot.target_theta)
+            # robot.node.get_logger().info("Now switching to second turn state")
 
-            self.state = "TURN_2"
+            robot.state = "TURN_2"
 
-    elif self.state == "TURN_2":
-        self.node.get_logger().info("Second turn state")
+    elif robot.state == "TURN_2":
+        # robot.node.get_logger().info("Second turn state")
 
-        error = normalize_angle(self.target_theta - self.pose.theta)
+        error = normalize_angle(robot.target_theta - robot.pose.theta)
         if abs(error) > 0.05:
-            msg.angular.z = max(min(self.pd_theta.update(self.pose.theta), 0.3), -0.2)
+            msg.angular.z = max(min(robot.pd_theta.update(robot.pose.theta), 0.3), -0.2)
             msg.linear.x = 0.0
-            self.vel_pub.publish(msg)
+            robot.vel_pub.publish(msg)
         else:
             msg.angular.z = 0.0
-            self.vel_pub.publish(msg)
-            self.sweep_direction *= -1
-            self.state = "FORWARD"
-            self.node.get_logger().info(
-                f"Completed one sweep, switching back to forward"
-            )
+            robot.vel_pub.publish(msg)
+            robot.sweep_direction *= -1
+            robot.state = "FORWARD"
+            # robot.node.get_logger().info(
+            #     f"Completed one sweep, switching back to forward"
+            # )
     else:
         msg.angular.z = 0.0
         msg.linear.x = 0.0
-        self.vel_pub.publish(msg)
-        self.node.get_logger().info(f"Idle state")
+        robot.vel_pub.publish(msg)
+        robot.node.get_logger().info(f"Idle state")
