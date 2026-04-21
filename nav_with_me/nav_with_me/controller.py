cf186aa6e93de854d909c155c2087b747f9e9721
Joud2003 <joud.salhi10@gmail.com>
2026-04-21 20:30:11 +0100

    Move constants to own class

--- nav_with_me/nav_with_me/controller.py
+++ nav_with_me/nav_with_me/controller.py
@@ -1,106 +1,124 @@
 from math import pi
 import numpy as np
+from nav_with_me.utils import PIDController
 
 
-def normalize_angle(angle):
-    while angle > pi:
-        angle -= 2 * pi
-    while angle < -pi:
-        angle += 2 * pi
-    return angle
+class MotionController:
+    def __init__(self, robot):
+        self.pd_x = PIDController(1.0, 0.1, 1.0)
+        self.pd_theta = PIDController(1.0, 0.5, 0.0)
+        self.robot = robot
+        self.sweep_direction = -1  # 1 = left, -1 = right
+        self.state = "FORWARD"
+        self.lane_width = 1.0  # lateral shift distance
+        self.forward_speed = 0.3
+        self.wall_threshold = 0.5
+        self.start_x = 0.0
+        self.start_y = 0.0
+        self.target_theta = 0.0
 
+    def normalize_angle(self, angle):
+        while angle > pi:
+            angle -= 2 * pi
+        while angle < -pi:
+            angle += 2 * pi
+        return angle
 
-def handleControl(robot, msg):
-    front_dist = np.mean(robot.front_readings)
-    robot.pd_x.setPoint(front_dist)
-    if front_dist < robot.wall_threshold:
-        msg.linear.x = 0.0
-        robot.vel_pub.publish(msg)
-    if robot.state == "FORWARD":
-        # robot.node.get_logger().info("Forward state")
-
-        if front_dist > robot.wall_threshold:
-            linear_v = robot.pd_x.update(robot.wall_threshold)
-            if abs(linear_v) > robot.forward_speed:
-                linear_v = robot.forward_speed * np.sign(linear_v)
-            msg.linear.x = abs(linear_v)
-            msg.angular.z = 0.0
-            robot.vel_pub.publish(msg)
-
-        else:
+    def handleControl(self, robot, msg):
+        front_dist = np.mean(robot.front_readings)
+        self.pd_x.setPoint(front_dist)
+        if front_dist < self.wall_threshold:
             msg.linear.x = 0.0
             robot.vel_pub.publish(msg)
+        if self.state == "FORWARD":
+            # robot.node.get_logger().info("Forward state")
 
-            robot.target_theta = normalize_angle(
-                robot.pose.theta + robot.sweep_direction * (pi / 2)
-            )
-            robot.pd_theta.setPoint(robot.target_theta)
-            # robot.node.get_logger().info("Now switching to first turn state")
-            robot.state = "TURN_1"
+            if front_dist > self.wall_threshold:
+                linear_v = self.pd_x.update(self.wall_threshold)
+                if abs(linear_v) > self.forward_speed:
+                    linear_v = self.forward_speed * np.sign(linear_v)
+                msg.linear.x = abs(linear_v)
+                msg.angular.z = 0.0
+                robot.vel_pub.publish(msg)
 
-    elif robot.state == "TURN_1":
-        # robot.node.get_logger().info("First turn state")
-        error = normalize_angle(robot.target_theta - robot.pose.theta)
-        if abs(error) > 0.05:
-            msg.angular.z = max(min(robot.pd_theta.update(robot.pose.theta), 0.3), -0.2)
-            msg.linear.x = 0.0
-            robot.vel_pub.publish(msg)
+            else:
+                msg.linear.x = 0.0
+                robot.vel_pub.publish(msg)
 
+                self.target_theta = self.normalize_angle(
+                    robot.pose.theta + self.sweep_direction * (pi / 2)
+                )
+                self.pd_theta.setPoint(self.target_theta)
+                # robot.node.get_logger().info("Now switching to first turn state")
+                self.state = "TURN_1"
+
+        elif self.state == "TURN_1":
+            robot.node.get_logger().info("First turn state")
+            error = self.normalize_angle(self.target_theta - robot.pose.theta)
+            if abs(error) > 0.05:
+                msg.angular.z = max(
+                    min(self.pd_theta.update(robot.pose.theta), 0.3), -0.2
+                )
+                msg.linear.x = 0.0
+                robot.vel_pub.publish(msg)
+
+            else:
+                msg.angular.z = 0.0
+                robot.vel_pub.publish(msg)
+                self.start_x = robot.pose.x
+                self.start_y = robot.pose.y
+                self.pd_x.setPoint(robot.pose.x + self.lane_width)
+                robot.node.get_logger().info(f"Now switching to shift state")
+                self.state = "SHIFT"
+
+        elif self.state == "SHIFT":
+            robot.node.get_logger().info("Shift state")
+            dx = robot.pose.x - self.start_x
+            dy = robot.pose.y - self.start_y
+            dist = np.sqrt(dx**2 + dy**2)
+            robot.node.get_logger().info(f"error is {self.lane_width - dist}")
+            error = self.lane_width - dist
+            if abs(error) > 0.05:
+                linear_v = self.pd_x.update(dist)
+                if abs(linear_v) > self.forward_speed:
+                    linear_v = self.forward_speed * np.sign(linear_v)
+
+                msg.linear.x = abs(linear_v)
+                msg.angular.z = 0.0
+                robot.vel_pub.publish(msg)
+
+            else:
+                msg.linear.x = 0.0
+                msg.angular.z = 0.0
+                robot.vel_pub.publish(msg)
+                self.target_theta = -1 * self.normalize_angle(
+                    robot.pose.theta + self.sweep_direction * (pi / 2)
+                )
+                self.pd_theta.setPoint(self.target_theta)
+                robot.node.get_logger().info("Now switching to second turn state")
+
+                self.state = "TURN_2"
+
+        elif self.state == "TURN_2":
+            robot.node.get_logger().info("Second turn state")
+
+            error = self.normalize_angle(self.target_theta - robot.pose.theta)
+            if abs(error) > 0.05:
+                msg.angular.z = max(
+                    min(self.pd_theta.update(robot.pose.theta), 0.3), -0.2
+                )
+                msg.linear.x = 0.0
+                robot.vel_pub.publish(msg)
+            else:
+                msg.angular.z = 0.0
+                robot.vel_pub.publish(msg)
+                self.sweep_direction *= -1
+                self.state = "FORWARD"
+                robot.node.get_logger().info(
+                    f"Completed one sweep, switching back to forward"
+                )
         else:
             msg.angular.z = 0.0
-            robot.vel_pub.publish(msg)
-            robot.start_x = robot.pose.x
-            robot.start_y = robot.pose.y
-            robot.pd_x.setPoint(robot.pose.x + robot.lane_width)
-            # robot.node.get_logger().info(f"Now switching to shift state")
-            robot.state = "SHIFT"
-
-    elif robot.state == "SHIFT":
-        # robot.node.get_logger().info("Shift state")
-        dx = robot.pose.x - robot.start_x
-        dy = robot.pose.y - robot.start_y
-        dist = np.sqrt(dx**2 + dy**2)
-
-        error = robot.lane_width - dist
-        if abs(error) > 0.05:
-            linear_v = robot.pd_x.update(dist)
-            if abs(linear_v) > robot.forward_speed:
-                linear_v = robot.forward_speed * np.sign(linear_v)
-
-            msg.linear.x = abs(linear_v)
-            msg.angular.z = 0.0
-            robot.vel_pub.publish(msg)
-
-        else:
-            msg.linear.x = 0.0
-            msg.angular.z = 0.0
-            robot.vel_pub.publish(msg)
-            robot.target_theta = -1 * normalize_angle(
-                robot.pose.theta + robot.sweep_direction * (pi / 2)
-            )
-            robot.pd_theta.setPoint(robot.target_theta)
-            # robot.node.get_logger().info("Now switching to second turn state")
-
-            robot.state = "TURN_2"
-
-    elif robot.state == "TURN_2":
-        # robot.node.get_logger().info("Second turn state")
-
-        error = normalize_angle(robot.target_theta - robot.pose.theta)
-        if abs(error) > 0.05:
-            msg.angular.z = max(min(robot.pd_theta.update(robot.pose.theta), 0.3), -0.2)
             msg.linear.x = 0.0
             robot.vel_pub.publish(msg)
-        else:
-            msg.angular.z = 0.0
-            robot.vel_pub.publish(msg)
-            robot.sweep_direction *= -1
-            robot.state = "FORWARD"
-            # robot.node.get_logger().info(
-            #     f"Completed one sweep, switching back to forward"
-            # )
-    else:
-        msg.angular.z = 0.0
-        msg.linear.x = 0.0
-        robot.vel_pub.publish(msg)
-        robot.node.get_logger().info(f"Idle state")
+            robot.node.get_logger().info(f"Idle state")
--- nav_with_me/nav_with_me/robot_navigation.py
+++ nav_with_me/nav_with_me/robot_navigation.py
@@ -2,13 +2,12 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 import threading
-from .controller import handleControl
+from .controller import MotionController
 import rclpy
 from tf_transformations import euler_from_quaternion
-from nav_msgs.msg import OccupancyGrid, Odometry
+from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import Twist, Pose2D
 from sensor_msgs.msg import LaserScan
-from .utils import PIDController
 from tf2_ros import Buffer, TransformListener
 
 
@@ -20,33 +19,19 @@ class Turtlebot3:
         self.vel_pub = self.node.create_publisher(Twist, "cmd_vel", 10)
         self.rate = self.node.create_rate(1)
         self.timer = self.node.create_timer(0.1, self.update_pose)  # 10 Hz
-
+        self.controller = MotionController(self)
         t = threading.Thread(target=rclpy.spin, args=(self.node,), daemon=True)
         t.start()
-        self.pose = Pose2D()
-        self.logging_counter = 0
-        self.trajectory = list()
         self.lidar_sub = self.node.create_subscription(
             LaserScan, "scan", self.scan_callback, 10
         )
         self.map_sub = self.node.create_subscription(
             OccupancyGrid, "map", self.map_callback, 10
         )
+        self.pose = Pose2D()
+        self.trajectory = list()
         self.map_data = None
-        self.pd_x = PIDController(1.0, 0.1, 1.0)
-        self.pd_theta = PIDController(1.0, 0.5, 0.0)
         self.front_readings = [4.0]
-        self.state = "FORWARD"
-        self.sweep_direction = -1  # 1 = left, -1 = right
-        self.lane_width = 1.0  # lateral shift distance
-        self.forward_speed = 0.3
-        self.turn_speed = 0.3
-        self.wall_threshold = 0.5
-
-        self.start_x = 0.0
-        self.start_y = 0.0
-        self.start_theta = 0.0
-        self.target_theta = 0.0
         self.resolution = 0.0
         self.trajectory = list()
         self.tf_buffer = Buffer()
@@ -55,7 +40,7 @@ class Turtlebot3:
     def run(self):
         msg = Twist()
         while rclpy.ok():
-            handleControl(self, msg)
+            self.controller.handleControl(self, msg)
             self.rate.sleep()
 
     def update_pose(self):
@@ -76,9 +61,6 @@ class Turtlebot3:
                 transform.transform.rotation.w,
             ]
         )[2]
-        self.node.get_logger().info(
-            f"Pose from slam is x: {self.pose.x}, y: {self.pose.y}, theta: {self.pose.theta}"
-        )
         self.trajectory.append([self.pose.x, self.pose.y])
 
     def scan_callback(self, msg):
@@ -99,12 +81,8 @@ class Turtlebot3:
         self.origin_y = msg.info.origin.position.y
 
         self.map_data = np.array(msg.data).reshape((self.map_height, self.map_width))
-
         self.node.get_logger().info(
             f"Map received {self.map_width} x {self.map_height}"
-        )
-        transform = self.tf_buffer.lookup_transform(
-            "map", "base_link", rclpy.time.Time()  # target frame  # robot frame
         )
 
 
