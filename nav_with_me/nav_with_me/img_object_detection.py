69175d6c09a537a5562272a98f8696f6977001ef
Joud2003 <joud.salhi10@gmail.com>
2026-05-10 16:43:30 +0100

    Fix object shape and return fake objects

--- nav_with_me/nav_with_me/img_object_detection.py
+++ nav_with_me/nav_with_me/img_object_detection.py
@@ -30,9 +30,32 @@ class ImageProcessor:
             x1, y1, x2, y2, conf, cls_id = det
             if conf > CONFIDENCE_THRESHOLD:
                 class_name = self.robot.yolo_model.names[int(cls_id)]
-                detected_objects.append((class_name, conf))
                 if class_name in HARMFUL_OBJECTS:
                     self.robot.node.get_logger().warn(
                         f"Harmful object detected: {class_name} with confidence {conf:.2f}"
                     )
+                    new_object = {
+                        "class": class_name,
+                        "pose": self.robot.estimated_pose,
+                        "conf": conf,
+                        "count": 1,
+                    }
+                    detected_objects.append(new_object)
+
         return detected_objects
+
+    def fake_detections(self, interval=0.1):
+        if self.robot.estimated_pose is None:
+            return []
+
+        x, y, theta = self.robot.estimated_pose
+        array_of_objects = []
+        for i in range(0, 20, 1):
+            new_object = {
+                "class": "knife",
+                "pose": [x + 0.2 + (i * interval), y + 0.1 + (i * interval), theta],
+                "conf": 0.9 - (i * interval * 0.1),
+                "count": 1,
+            }
+            array_of_objects.append(new_object)
+        return array_of_objects
